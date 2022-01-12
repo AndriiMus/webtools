198b1a527bffa48174dfdb2768b876c1ed606d65
MakeTSGreatAgain <andrii.mus00@gmail.com>
2022-01-12 20:36:02 +0200

    ss

--- lesson6/task1/webpack.config.js
+++ lesson6/task1/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
       },
       {
         test: /.scss$/,
-        use: ['scss-loader', 'style-loader', 'css-loader'],
+        use: ['sass-loader', 'style-loader', 'css-loader'],
       },
       {
         test: /.(jpg|png)$/,
