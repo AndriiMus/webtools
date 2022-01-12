46fbb81ed27aef9aa787846d12720ba061a2f336
MakeTSGreatAgain <andrii.mus00@gmail.com>
2022-01-12 20:29:32 +0200

    sass

---
+++ lesson6/task1/webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /.(jpg|png)$/,
+        use: ['url-loader'],
+      },
+    ],
+  },
+};
