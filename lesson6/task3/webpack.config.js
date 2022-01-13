fc48f1d9fcea29edaea496cfc3df412cbc9eddb8
MakeTSGreatAgain <andrii.mus00@gmail.com>
2022-01-13 13:02:03 +0200

    new task2

---
+++ lesson6/task3/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        use: ['babel-loader'],
+      },
+      {
+        test: /.s?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /.(jpg|png)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              outputPath: 'images',
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
